fbb4e6481e3fde6dcdc7059e6bdffcb7eeedb6d5
Alice <alice.vegliach@gmail.com>
2020-05-11 18:41:58 +0200

    fixed ex2

---
+++ src/lab_02_b.R
@@ -0,0 +1,39 @@
+#observed information matrix
+jhat <- weib.y.mle$hessian
+mle.se <-sqrt(diag(solve(jhat)))
+# c.i.
+alpha <- 1- 0.95
+wald.ci.gamma <- gammahat + qnorm(1- alpha/2) * mle.se[1] *c(-1,1)
+wald.ci.beta <- betahat + qnorm(1- alpha/2) * mle.se[2] *c(-1,1)
+
+wald.ci.gamma
+
+# profile likeligood function
+log_lik_weibull_profile  <- function(data, gamma){
+  beta.gamma <- mean(data^gamma)^(1/gamma)
+  log_lik_weibull( data, c(gamma, beta.gamma) )
+}
+
+log_lik_weibull_profile_v <-Vectorize(log_lik_weibull_profile, 'gamma'  )
+
+# plot 
+plot(function(x) -log_lik_weibull_profile_v(data=y, x)+weib.y.mle$value, 
+     from=0.1,to=15,xlab=expression(gamma),  ylab='profile relative log likelihood',ylim=c(-8,0))
+
+segments(wald.ci.gamma[1], -log_lik_weibull_profile_v(y, wald.ci.gamma[1])+weib.y.mle$value,
+         wald.ci.gamma[1], -log_lik_weibull_profile_v(y, wald.ci.gamma[1]), col="red", lty=2)
+
+segments(wald.ci.gamma[2], -log_lik_weibull_profile_v(y, wald.ci.gamma[2])+weib.y.mle$value,
+         wald.ci.gamma[2], -log_lik_weibull_profile_v(y, wald.ci.gamma[2]), col="red", lty=2)
+
+points(wald.ci.gamma[1],  -log_lik_weibull_profile_v(y, wald.ci.gamma[1])+weib.y.mle$value,
+       pch=16, col=2, cex=1.5)
+
+points(wald.ci.gamma[2], -log_lik_weibull_profile_v(y, wald.ci.gamma[2])+weib.y.mle$value,
+       pch=16, col=2, cex=1.5)
+
+segments( wald.ci.gamma[1], -8.1, wald.ci.gamma[2], -8.1, col="red", lty =1, lwd=2  )
+
+abline(v=gammahat, col=4, lwd=1, lty=2)
+
+text(7,-7.5,"95% Wald-type CI",col=2)
